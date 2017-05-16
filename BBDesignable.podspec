8d03670559a03cc082f163c33f5c5bc5aa3b6416
Benjamin Bourasseau <benjboum@hotmail.fr>
2017-05-16 13:25:41 -0400

    Adding borderColor and borderWidth

--- BBDesignable.podspec
+++ BBDesignable.podspec
@@ -2,13 +2,13 @@ Pod::Spec.new do |s|
 
 # 1
 s.platform = :ios
-s.ios.deployment_target = '8.0'
+s.ios.deployment_target = '9.0'
 s.name = "BBDesignable"
 s.summary = "Some cool functionalities directly inside Storyboards and xibs"
 s.requires_arc = true
 
 # 2
-s.version = "0.2.0"
+s.version = "0.3.0"
 
 # 3
 s.license = { :type => "MIT", :file => "LICENSE" }
--- BBDesignable/BBDesignable/UIView/UIView.swift
+++ BBDesignable/BBDesignable/UIView/UIView.swift
@@ -10,12 +10,34 @@ import Foundation
 import UIKit
 
 public extension UIView {
+    
     @IBInspectable public var cornerRadius: CGFloat {
         get {
             return layer.cornerRadius
         }
         set {
             layer.cornerRadius = newValue
+        }
+    }
+    
+    @IBInspectable public var borderColor: UIColor? {
+        get {
+            guard let color = layer.borderColor else {
+                return nil
+            }
+            return UIColor(cgColor: color)
+        }
+        set {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+    
+    @IBInspectable public var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
         }
     }
 }
